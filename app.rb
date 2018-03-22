1939d2f433356a7526ca71d8aa59e6adcfd76c38
itggot-Romina-Asadi <romina.asadi@itggot.se>
2018-03-22 10:11:34 +0100

    La till routes och cssfil

--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
 class App < Sinatra::Base
 
 	get '/' do
-		"Hello, Grillkorv!"
+		slim(:index)
+	end
+
+	get '/signin' do
+		slim(:signin)
+	end
+
+	get '/signup' do
+		slim(:signup)
+	end
+
+	get '/mypage' do
+		slim(:mypage)
+	end
+
+	get '/group' do
+		slim(:members)
+	end
+
+	get '/todo' do
+		slim(:todo)
 	end
 
 end           
