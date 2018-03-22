3bb36ae33864358cacb81451a1a2ae062bf87ae9
itggot-Romina-Asadi <romina.asadi@itggot.se>
2018-03-22 10:36:58 +0100

    Skapade signin och signout

--- app.rb
+++ app.rb
@@ -24,4 +24,17 @@ class App < Sinatra::Base
 		slim(:todo)
 	end
 
+	get '/error' do
+		slim(:error, locals:{msg:session[:message]})
+	end
+
+	post '/signin' do
+		db = SQLite3::Databse.new("slutprojekt.db")
+		username = params["username"]
+		password = params["password"]
+		user_id = db.execute("SELECT user_id FROM signin WHERE username='#{username}'")
+		password_digest = db.execute("SELECT password FROM signin WHERE username='#{username}'").join
+		password_digest = BCrypt::Password.new(password_digest)
+	end
+
 end           
