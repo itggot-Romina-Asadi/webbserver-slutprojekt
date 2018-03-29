3bd8f682077f5a03a388838ccd25612960fbd9fb
itggot-Romina-Asadi <romina.asadi@itggot.se>
2018-03-29 10:39:39 +0200

    Sign in och sign up
    - Kryptera lösenord
    - Kopplade dem till databasen

--- app.rb
+++ app.rb
@@ -29,12 +29,44 @@ class App < Sinatra::Base
 	end
 
 	post '/signin' do
-		db = SQLite3::Databse.new("slutprojekt.db")
+		db = SQLite3::Database.new("db/slutprojekt.db")
 		username = params["username"]
 		password = params["password"]
-		user_id = db.execute("SELECT user_id FROM signin WHERE username='#{username}'")
-		password_digest = db.execute("SELECT password FROM signin WHERE username='#{username}'").join
+		user_id = db.execute("SELECT user_id FROM users WHERE username='#{username}'")
+		password_digest = db.execute("SELECT password FROM users WHERE username='#{username}'").join
 		password_digest = BCrypt::Password.new(password_digest)
+		if password_digest == password
+			session[:username] = username
+			session[:user_id] = user_id
+			redirect('/mypage')
+		else
+			redirect('/')
+		end
 	end
 
-end           
+	post '/signup' do
+		db = SQLite3::Database.new("db/slutprojekt.db")
+		username = params["username"]
+		password = params["password"]
+		password2 = params["password2"]
+		password_digest = BCrypt::Password.create("#{password}")
+		if username.length > 0 
+			if password == password2 && password.length > 0
+				begin
+					db.execute("INSERT INTO users (username, password) VALUES (?,?)", [username, password_digest])
+				rescue 
+					session[:message] = "The username is unavailable"
+					redirect('/error')
+				end
+				redirect('/signin')
+			else
+				session[:message] = "Password unavailable"
+				redirect('/error')
+			end
+		else
+			sesson[:message] = "The username is unavailable"
+			redirect('/error')
+		end
+	end
+
+end
