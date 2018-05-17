7067cfbf4ba0e0014a0d37ff1088bbb6153392a8
itggot-Romina-Asadi <romina.asadi@itggot.se>
2018-05-17 10:35:00 +0200

    Klar med module

--- app.rb
+++ app.rb
@@ -31,8 +31,7 @@ class App < Sinatra::Base
 
 	get '/grouplist' do
 		user_id = session[:user_id]
-		db = SQLite3::Database.new("db/slutprojekt.db")
-		groupname = db.execute("SELECT * FROM groups WHERE user_id=?", [user_id])
+		groupname = get_groupname(user_id)
 		slim(:grouplist, locals:{groupname:groupname} )
 	end 
 
@@ -43,10 +42,9 @@ class App < Sinatra::Base
 	get '/grouppage/:id' do
 		user_id = session[:user_id]
 		group_id = params[:id].to_i
-		db = SQLite3::Database.new("db/slutprojekt.db")
-		groupname = db.execute("SELECT name FROM groups WHERE user_id=?", [user_id])
-		members = db.execute("SELECT username FROM users WHERE user_id IN (SELECT user_id FROM groups WHERE group_id = ?)", [group_id])
-		username = db.execute("SELECT username FROM users WHERE user_id = ?", [user_id]).join
+		groupname = get_groupname(user_id)
+		members = get_members(group_id)
+		username = get_username(user_id)
 		if members.include?([username])
 			slim(:grouppage, locals:{group:groupname, users:members, group_id:group_id})
 		else
@@ -60,11 +58,10 @@ class App < Sinatra::Base
 	end
 
 	post '/signin' do
-		db = SQLite3::Database.new("db/slutprojekt.db")
 		username = params["username"]
 		password = params["password"]
-		user_id = db.execute("SELECT user_id FROM users WHERE username='#{username}'")
-		password_digest = db.execute("SELECT password FROM users WHERE username='#{username}'").join
+		user_id = get_user_id(username)
+		password_digest = get_password(username)
 		password_digest = BCrypt::Password.new(password_digest)
 		if password_digest == password
 			session[:username] = username
@@ -76,22 +73,18 @@ class App < Sinatra::Base
 	end
 
 	post '/signup' do
-		db = SQLite3::Database.new("db/slutprojekt.db")
 		username = params["username"]
 		password = params["password"]
 		password2 = params["password2"]
 		password_digest = BCrypt::Password.create("#{password}")
-		usernames = db.execute("SELECT username FROM users")
-		p usernames
-		p [username]
-		p usernames.include?([username])
+		usernames = get_usernames(username)
 		if username.length > 3
 			if password == password2 && password.length > 0
 				if usernames.include?([username])
 					session[:message] = "The username is unavailable"
 					redirect('/error')
 				else
-					db.execute("INSERT INTO users (username, password) VALUES (?,?)", [username, password_digest])
+					create_user(username, password_digest)
 					redirect('/signin')
 				end
 			else
@@ -106,9 +99,8 @@ class App < Sinatra::Base
 
 	post '/group' do
 		user_id = session[:user_id]
-		db = SQLite3::Database.new("db/slutprojekt.db")
 		groupname = params["name"]
-		db.execute("INSERT INTO groups (user_id, name) VALUES (?, ?)", [user_id, groupname])
+		create_group(user_id, groupname)
 		redirect('/grouplist')
 	end
 
--- module.rb
+++ module.rb
@@ -7,4 +7,39 @@ module Database
         db = connect()
         return db.execute("SELECT * FROM groups WHERE user_id=?", [user_id])
     end
+
+    def get_members(group_id)
+        db = connect()
+        return db.execute("SELECT username FROM users WHERE user_id IN (SELECT user_id FROM groups WHERE group_id = ?)", [group_id])
+    end
+
+    def get_username(user_id)
+        db = connect()
+        return db.execute("SELECT username FROM users WHERE user_id = ?", [user_id]).join
+    end
+
+    def get_user_id(username)
+        db = connect()
+        return db.execute("SELECT user_id FROM users WHERE username='#{username}'")
+    end
+
+    def get_password(username)
+        db = connect()
+        return db.execute("SELECT password FROM users WHERE username='#{username}'").join
+    end
+
+    def get_usernames(username)
+        db = connect()
+        return db.execute("SELECT username FROM users")
+    end
+
+    def create_user(username, password_digest)
+        db = connect()
+        return db.execute("INSERT INTO users (username, password) VALUES (?,?)", [username, password_digest])
+    end
+
+    def create_group(user_id, groupname)
+        db = connect()
+        return db.execute("INSERT INTO groups (user_id, name) VALUES (?, ?)", [user_id, groupname])
+    end
 end
